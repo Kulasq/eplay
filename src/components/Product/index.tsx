872cbd5227c219834b7bb00087427ec9e53b3109
Kulasq <lucasdantas_11@hotmail.com>
2025-03-12 13:32:59 -0300

    componentizando a lista de produtos

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,19 +1,35 @@
 import Tag from '../Tag'
 
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Titulo, Infos } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => (
   <Card>
-    <img src="https://placehold.co/222x250" />
-    <Titulo>Nome do jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quidem
-      culpa explicabo debitis optio exercitationem libero. Aperiam nesciunt
-      atque tempore cumque soluta aliquid praesentium cupiditate, ducimus
-      laboriosam, iusto ex commodi?
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
